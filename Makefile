7a6b2f0c8b8adbc4f5b636883dcff7f5657e7890
Yev Barkalov <yevbar1729@gmail.com>
2024-09-01 17:07:27 -0400

    fix os call

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 generate:
-	pipenv run python -m build
+	rm -rf dist && pipenv run python -m build
 release:
 	pipenv run python -m twine upload dist/*
--- src/lusid/mac_db.py
+++ src/lusid/mac_db.py
@@ -5,4 +5,4 @@ def get_db_path():
     """
     Returns path to the chat.db file iMessage uses under the hood
     """
-    return os.expanduser("~") + "/Library/Messages/chat.db"
+    return os.path.expanduser("~") + "/Library/Messages/chat.db"
