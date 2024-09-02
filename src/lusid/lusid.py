6b66df52756ddd207f844434df7bbfd297ce9394
Yev Barkalov <yevbar1729@gmail.com>
2024-09-02 18:15:34 -0400

    does this fix it?

--- src/lusid/lusid.py
+++ src/lusid/lusid.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from requests import post
 from time import sleep
 
-from py_imessage import imessage
-import mac_imessage
-from imessage_reader import fetch_data
+from .py_imessage import imessage
+import .mac_imessage
+from .imessage_reader import fetch_data
 
 from .mac_db import get_db_path
 
